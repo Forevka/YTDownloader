603e63b73fb420f9c98828069d69cd2470fbf9b1
OneZeroZeroOneOne <50696667+OneZeroZeroOneOne@users.noreply.github.com>
2019-12-24 13:57:32 +0200

    na

---
+++ web.backend/web.py
@@ -0,0 +1,26 @@
+from aiohttp import web
+from aiohttp_apispec import (setup_aiohttp_apispec, validation_middleware)
+from loguru import logger
+from utils.patch import patch; patch()
+from utils.base_model import serialize_middleware
+from utils.generic_request import generic_request_middleware
+import routes
+
+if __name__ == '__main__':
+    app = web.Application()
+
+    routes.register(app)
+
+    app.middlewares.append(validation_middleware)
+    app.middlewares.append(serialize_middleware)
+    app.middlewares.append(generic_request_middleware)
+
+    setup_aiohttp_apispec(
+        app=app,
+        title="My Documentation",
+        version="v1",
+        url="/api/docs/swagger.json",
+        swagger_path="/api/docs",
+    )
+
+    web.run_app(app, port=9999)
