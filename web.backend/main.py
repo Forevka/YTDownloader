fff92bfc13b70843472ec6cce13ed6609d89433a
bohdan.lushchyk <bohdan.lushchyk@devcom.com>
2019-12-26 14:58:09 +0200

    docker and fix requirements

--- web.backend/main.py
+++ web.backend/main.py
@@ -1,7 +1,12 @@
+#ENV PYTHONPATH /app
+
+##docker build -t myimage .
+##docker run -d --name test -p 8080:8080 myimage
+
 import uvicorn
 from app import app
 import handlers.get_video_quality
 
 
-if __name__ == '__main__':
-    uvicorn.run(app, host='0.0.0.0', port=8000)
+#if __name__ == '__main__':
+#    uvicorn.run(app, host='0.0.0.0', port=8000)
